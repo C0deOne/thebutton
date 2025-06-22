a28aa4a9cd28f5fa5f5f42d2766c0116f286b6a3
C0deOne <david@chilterncomputers.net>
2025-06-22 17:22:05 +0100

    update3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import adapter from '@astrojs/adapter-static';
 
 export default defineConfig({
   site: 'https://c0deone.github.io/thebutton',
   base: '/thebutton/',
-  adapter: adapter(),
+  // No adapter needed for static deployment
 });
